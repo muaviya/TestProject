462905388cb9d982964aff6ea2029ad7a2dc2d76
Developer717 <ismaisan@mail.ru>
2017-03-23 09:37:44 +0300

    first commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Developer717 on 23.03.17.
+//  Copyright © 2017 DIIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.isNavigationBarHidden = true
+        
+        print("view did load")
+    }
+
+}
+
